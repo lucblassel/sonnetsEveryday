4a21ede6161de2422774c8b565f5e830eedfce94
luc blassel <luc.blassel@gmail.com>
2018-03-16 02:28:13 +0100

    addded tweet capability

---
+++ sonnets.py
@@ -0,0 +1,46 @@
+"""
+tweets a sonnet everyday
+"""
+import tweepy
+from credentials import *
+from time import sleep
+
+sonnetsPath = 'sonnets.txt'
+sleepTime = 24*1000
+
+def authenticate():
+    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessToken, accessTokenSecret)
+    api = tweepy.API(auth)
+
+    return api
+
+def loadSonnets(sonnetsPath):
+    sonnetsFile = open(sonnetsPath,'r')
+    sonnets = sonnetsFile.readlines()
+    sonnetsFile.close()
+
+    return sonnets
+
+def tweet(api,line):
+    try:
+        api.update(line)
+        sleep(20)
+    except tweepy.TweepError as e:
+        print(e.reason)
+        sleep(5)
+
+def loop():
+    lineCount = 0
+    sonnets = loadSonnets(sonnetsPath)
+    api = authenticate()
+
+    for line in sonnets:
+        if lineCount > 14:
+            sleep(sleepTime)
+            lineCount = 0
+
+        if line[0] != '#' and line[0]!='\n':
+            print(line)
+            tweet(api)
+            lineCount +=1
